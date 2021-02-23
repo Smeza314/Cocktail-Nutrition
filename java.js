b9ea81165646db6aff027067bf9df824fa54bb77
Kenneth Dy <kennethndy@yahoo.com>
2021-02-23 12:04:38 -0800

    added card container

--- java.js
+++ java.js
@@ -1,15 +1,15 @@
 
 
-axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => console.error(err))
+// axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch(err => console.error(err))
 
-axios.get('https://api.edamam.com/api/nutrition-data?app_id=c543d3f7&app_key=b07975353995fafd647e1d5c37e04dc0&ingr=1%20large%20apple')
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => console.error(err))
+// axios.get('https://api.edamam.com/api/nutrition-data?app_id=c543d3f7&app_key=b07975353995fafd647e1d5c37e04dc0&ingr=1%20large%20apple')
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch(err => console.error(err))
 
 
