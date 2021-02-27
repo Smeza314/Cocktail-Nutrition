5b6d7ffdcd17e999872c5842b94e5d1202c6238c
Kenneth Dy <kennethndy@yahoo.com>
2021-02-26 23:39:33 -0800

    added local storage and separated click events

--- apps.js
+++ apps.js
@@ -56,6 +56,7 @@ const getDrink = (drinkSearch) => {
         drinkList.push(drank)
       }
       console.log(drinkList)
+      localStorage.setItem('drinkList', JSON.stringify(drinkList))
 
       document.getElementById('cocktail-preview').innerHTML = ''
 
@@ -72,145 +73,145 @@ const getDrink = (drinkSearch) => {
         </div>
       </div>
       `}
-      document.addEventListener('click', event => {
-        if (event.target.classList.contains('cImg') || event.target.classList.contains('card-content')) {
+      // document.addEventListener('click', event => {
+      //   if (event.target.classList.contains('cImg') || event.target.classList.contains('card-content')) {
 
-          let index = event.target.dataset.index
-          let drinkIngList = ''
-          let drinkIngStrList = []
+      //     let index = event.target.dataset.index
+      //     let drinkIngList = ''
+      //     let drinkIngStrList = []
 
-          drinkList[index].ingredients.forEach(elem => {
-            if (elem.measure === null) { elem.measure = '' }
-            drinkIngList += ` ${elem.measure} ${elem.ingredient},`
-            let strIngr = `${elem.measure} ${elem.ingredient}`
-            drinkIngStrList.push(strIngr)
-          });
-          let editedDrinkIngList = drinkIngList.slice(0, -1)
+      //     drinkList[index].ingredients.forEach(elem => {
+      //       if (elem.measure === null) { elem.measure = '' }
+      //       drinkIngList += ` ${elem.measure} ${elem.ingredient},`
+      //       let strIngr = `${elem.measure} ${elem.ingredient}`
+      //       drinkIngStrList.push(strIngr)
+      //     });
+      //     let editedDrinkIngList = drinkIngList.slice(0, -1)
 
-          let nutritionxRecipe = {
-            query: editedDrinkIngList
-          }
+      //     let nutritionxRecipe = {
+      //       query: editedDrinkIngList
+      //     }
 
-          let headers = {
-            'x-app-id': '1134500c',
-            'x-app-key': 'facd7a17165e187875686dccb6161af8',
-            'x-remote-user-id': 0
-          }
+      //     let headers = {
+      //       'x-app-id': '1134500c',
+      //       'x-app-key': 'facd7a17165e187875686dccb6161af8',
+      //       'x-remote-user-id': 0
+      //     }
 
-          console.log(editedDrinkIngList)
-          axios.post('https://trackapi.nutritionix.com/v2/natural/nutrients', nutritionxRecipe, { headers })
-            .then(res => {
-              console.log(res)
-              let totalNutr = res.data.foods
-              let calPercent = 0
-              let sugarPercent = 0
-              let carbPercent = 0
-              let sodiumPercent = 0
-              let fatPercent = 0
-              let cal = 0
-              let sugars = 0
-              let carbs = 0
-              let fat = 0
-              let sodium = 0
+      //     console.log(editedDrinkIngList)
+      //     axios.post('https://trackapi.nutritionix.com/v2/natural/nutrients', nutritionxRecipe, { headers })
+      //       .then(res => {
+      //         console.log(res)
+      //         let totalNutr = res.data.foods
+      //         let calPercent = 0
+      //         let sugarPercent = 0
+      //         let carbPercent = 0
+      //         let sodiumPercent = 0
+      //         let fatPercent = 0
+      //         let cal = 0
+      //         let sugars = 0
+      //         let carbs = 0
+      //         let fat = 0
+      //         let sodium = 0
 
-              totalNutr.forEach(elem => {
-                cal += elem.nf_calories
-                sugars += elem.nf_sugars
-                carbs += elem.nf_total_carbohydrate
-                fat += elem.nf_total_fat
-                sodium += elem.nf_sodium
-              })
+      //         totalNutr.forEach(elem => {
+      //           cal += elem.nf_calories
+      //           sugars += elem.nf_sugars
+      //           carbs += elem.nf_total_carbohydrate
+      //           fat += elem.nf_total_fat
+      //           sodium += elem.nf_sodium
+      //         })
 
-              calPercent = (cal / 2000)*100
-              sugarPercent = (sugars / 100)*100
-              carbPercent = (carbs / 275)*100
-              fatPercent = (fat / 70)*100
-              sodiumPercent = (sodium / 2300 )*100
-              console.log(calPercent)
-              console.log(cal)
-              console.log(sugars)
-              console.log(carbs)
-              console.log(fat)
-              console.log(sodium)
+      //         calPercent = (cal / 2000)*100
+      //         sugarPercent = (sugars / 100)*100
+      //         carbPercent = (carbs / 275)*100
+      //         fatPercent = (fat / 70)*100
+      //         sodiumPercent = (sodium / 2300 )*100
+      //         console.log(calPercent)
+      //         console.log(cal)
+      //         console.log(sugars)
+      //         console.log(carbs)
+      //         console.log(fat)
+      //         console.log(sodium)
 
-              document.getElementById('details').innerHTML = `  
-                <div class="row padding">
-                  <!-- first column with image and drink name -->
-                  <div class="col s12 m12 l12 xl4">
-                    <div class="deetsBox">
-                      <div class="txtCenter">
-                      <div class="row">
-                        <h1 class="nutri">${drinkList[index].name}</h1>
-                      </div>
-                      <div class="row">
-                      <img src="${drinkList[index].image}" alt="${drinkList[index].name}"
-                        class="drinkImg" id="drinkImg">
-                       </div>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- ingredients list and instructions -->
-                  <div class="col s12 m12 l12 xl4">
-                    <div class="deetsBox">
-                      <h4 class="ingr">Ingredients</h4>
-                      <ul class="ingrList">
-                        <li class="collection " id="ingredients"></li>
-                      </ul>
-                      <h4 class="ingr">Instructions</h4>
-                      <div class="instr" id="instructions">
-                        <p>${drinkList[index].instruction}</p>
-                      </div>
-                    </div>
-                  </div>
+      //         document.getElementById('details').innerHTML = `  
+      //           <div class="row padding">
+      //             <!-- first column with image and drink name -->
+      //             <div class="col s12 m12 l12 xl4">
+      //               <div class="deetsBox">
+      //                 <div class="txtCenter">
+      //                 <div class="row">
+      //                   <h1 class="nutri">${drinkList[index].name}</h1>
+      //                 </div>
+      //                 <div class="row">
+      //                 <img src="${drinkList[index].image}" alt="${drinkList[index].name}"
+      //                   class="drinkImg" id="drinkImg">
+      //                  </div>
+      //                 </div>
+      //               </div>
+      //             </div>
+      //             <!-- ingredients list and instructions -->
+      //             <div class="col s12 m12 l12 xl4">
+      //               <div class="deetsBox">
+      //                 <h4 class="ingr">Ingredients</h4>
+      //                 <ul class="ingrList">
+      //                   <li class="collection " id="ingredients"></li>
+      //                 </ul>
+      //                 <h4 class="ingr">Instructions</h4>
+      //                 <div class="instr" id="instructions">
+      //                   <p>${drinkList[index].instruction}</p>
+      //                 </div>
+      //               </div>
+      //             </div>
 
-                  <!-- nutritional information -->
-                  <div class="col s12 m12 l12 xl4">
-                    <div class="deetsBox txtCenter">
-                      <div class="nutriBox">
-                        <h1 class="nutri">Nutrients </h1>
-                      </div>
+      //             <!-- nutritional information -->
+      //             <div class="col s12 m12 l12 xl4">
+      //               <div class="deetsBox txtCenter">
+      //                 <div class="nutriBox">
+      //                   <h1 class="nutri">Nutrients </h1>
+      //                 </div>
 
-                      <div class="row nutrDetails">
-                        <div class="col rNutr">
-                          <h3 class="values">Calorie</h3>
-                          <p>${cal.toFixed(2)}kcal</p>
-                          <div class="w3-light-grey w3-round-xlarge">
-                            <div class="w3-container barFill w3-round-xlarge" style="width:${calPercent}%">${Math.floor(calPercent)}%</div>
-                          </div>
-                          <h3 class="values">Sugar</h3>
-                          <p>${sugars.toFixed(2)}g</p>
-                          <div class="w3-light-grey w3-round-xlarge">
-                            <div class="w3-container barFill w3-round-xlarge" style="width:${sugarPercent}%">${Math.floor(sugarPercent)}%</div>
-                          </div>
-                          <h3 class="values">Carbs</h3>
-                          <p>${carbs.toFixed(2)}g</p>
-                          <div class="w3-light-grey w3-round-xlarge">
-                            <div class="w3-container barFill w3-round-xlarge" style="width:${carbPercent}%">${Math.floor(carbPercent)}%</div>
-                          </div>
-                          <h3 class="values">Fat</h3>
-                          <p>${fat.toFixed(2)}g</p>
-                          <div class="w3-light-grey w3-round-xlarge">
-                            <div class="w3-container barFill w3-round-xlarge" style="width:${fatPercent}%">${Math.floor(fatPercent)}%</div>
-                          </div>
-                          <h3 class="values">Sodium</h3>
-                          <p>${sodium.toFixed(2)}mg</p>
-                          <div class="w3-light-grey w3-round-xlarge">
-                            <div class="w3-container barFill w3-round-xlarge" style="width:${sodiumPercent}%">${Math.floor(sodiumPercent)}%</div>
-                          </div>
+      //                 <div class="row nutrDetails">
+      //                   <div class="col rNutr">
+      //                     <h3 class="values">Calorie</h3>
+      //                     <p>${cal.toFixed(2)}kcal</p>
+      //                     <div class="w3-light-grey w3-round-xlarge">
+      //                       <div class="w3-container barFill w3-round-xlarge" style="width:${calPercent}%">${Math.floor(calPercent)}%</div>
+      //                     </div>
+      //                     <h3 class="values">Sugar</h3>
+      //                     <p>${sugars.toFixed(2)}g</p>
+      //                     <div class="w3-light-grey w3-round-xlarge">
+      //                       <div class="w3-container barFill w3-round-xlarge" style="width:${sugarPercent}%">${Math.floor(sugarPercent)}%</div>
+      //                     </div>
+      //                     <h3 class="values">Carbs</h3>
+      //                     <p>${carbs.toFixed(2)}g</p>
+      //                     <div class="w3-light-grey w3-round-xlarge">
+      //                       <div class="w3-container barFill w3-round-xlarge" style="width:${carbPercent}%">${Math.floor(carbPercent)}%</div>
+      //                     </div>
+      //                     <h3 class="values">Fat</h3>
+      //                     <p>${fat.toFixed(2)}g</p>
+      //                     <div class="w3-light-grey w3-round-xlarge">
+      //                       <div class="w3-container barFill w3-round-xlarge" style="width:${fatPercent}%">${Math.floor(fatPercent)}%</div>
+      //                     </div>
+      //                     <h3 class="values">Sodium</h3>
+      //                     <p>${sodium.toFixed(2)}mg</p>
+      //                     <div class="w3-light-grey w3-round-xlarge">
+      //                       <div class="w3-container barFill w3-round-xlarge" style="width:${sodiumPercent}%">${Math.floor(sodiumPercent)}%</div>
+      //                     </div>
                           
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              `
-              drinkIngStrList.forEach(elem => {
-                document.getElementById('ingredients').innerHTML += `
-                  <li class="collection-item">${elem}</li>
-                `
-              })
-            })
-            .catch(err => console.error(err))
+      //                   </div>
+      //                 </div>
+      //               </div>
+      //             </div>
+      //           </div>
+      //         `
+      //         drinkIngStrList.forEach(elem => {
+      //           document.getElementById('ingredients').innerHTML += `
+      //             <li class="collection-item">${elem}</li>
+      //           `
+      //         })
+      //       })
+      //       .catch(err => console.error(err))
 
           // drinkList[index].ingredients.forEach(elem => {
           //   if(elem.measure === null){elem.measure = '1 serving of'}
@@ -318,12 +319,157 @@ const getDrink = (drinkSearch) => {
           //     });
           //   })
           //   .catch(err => console.error(err))
-        }
-      })
+      //   }
+      // })
     })
     .catch(err => console.error(err))
 }
 
+
+
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('cImg') || event.target.classList.contains('card-content')) {
+
+    drinkList = JSON.parse(localStorage.getItem('drinkList'))
+    let index = event.target.dataset.index
+    let drinkIngList = ''
+    let drinkIngStrList = []
+
+    drinkList[index].ingredients.forEach(elem => {
+      if (elem.measure === null) { elem.measure = '' }
+      drinkIngList += ` ${elem.measure} ${elem.ingredient},`
+      let strIngr = `${elem.measure} ${elem.ingredient}`
+      drinkIngStrList.push(strIngr)
+    });
+    let editedDrinkIngList = drinkIngList.slice(0, -1)
+
+    let nutritionxRecipe = {
+      query: editedDrinkIngList
+    }
+
+    let headers = {
+      'x-app-id': '1134500c',
+      'x-app-key': 'facd7a17165e187875686dccb6161af8',
+      'x-remote-user-id': 0
+    }
+
+    console.log(editedDrinkIngList)
+    axios.post('https://trackapi.nutritionix.com/v2/natural/nutrients', nutritionxRecipe, { headers })
+      .then(res => {
+        console.log(res)
+        let totalNutr = res.data.foods
+        let calPercent = 0
+        let sugarPercent = 0
+        let carbPercent = 0
+        let sodiumPercent = 0
+        let fatPercent = 0
+        let cal = 0
+        let sugars = 0
+        let carbs = 0
+        let fat = 0
+        let sodium = 0
+
+        totalNutr.forEach(elem => {
+          cal += elem.nf_calories
+          sugars += elem.nf_sugars
+          carbs += elem.nf_total_carbohydrate
+          fat += elem.nf_total_fat
+          sodium += elem.nf_sodium
+        })
+
+        calPercent = (cal / 2000) * 100
+        sugarPercent = (sugars / 100) * 100
+        carbPercent = (carbs / 275) * 100
+        fatPercent = (fat / 70) * 100
+        sodiumPercent = (sodium / 2300) * 100
+        console.log(calPercent)
+        console.log(cal)
+        console.log(sugars)
+        console.log(carbs)
+        console.log(fat)
+        console.log(sodium)
+
+        document.getElementById('details').innerHTML = `  
+          <div class="row padding">
+            <!-- first column with image and drink name -->
+            <div class="col s12 m12 l12 xl4">
+              <div class="deetsBox">
+                <div class="txtCenter">
+                <div class="row">
+                  <h1 class="nutri">${drinkList[index].name}</h1>
+                </div>
+                <div class="row">
+                <img src="${drinkList[index].image}" alt="${drinkList[index].name}"
+                  class="drinkImg" id="drinkImg">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- ingredients list and instructions -->
+            <div class="col s12 m12 l12 xl4">
+              <div class="deetsBox">
+                <h4 class="ingr">Ingredients</h4>
+                <ul class="ingrList">
+                  <li class="collection " id="ingredients"></li>
+                </ul>
+                <h4 class="ingr">Instructions</h4>
+                <div class="instr" id="instructions">
+                  <p>${drinkList[index].instruction}</p>
+                </div>
+              </div>
+            </div>
+
+            <!-- nutritional information -->
+            <div class="col s12 m12 l12 xl4">
+              <div class="deetsBox txtCenter">
+                <div class="nutriBox">
+                  <h1 class="nutri">Nutrients </h1>
+                </div>
+
+                <div class="row nutrDetails">
+                  <div class="col rNutr">
+                    <h3 class="values">Calorie</h3>
+                    <p>${cal.toFixed(2)}kcal</p>
+                    <div class="w3-light-grey w3-round-xlarge">
+                      <div class="w3-container barFill w3-round-xlarge" style="width:${calPercent}%">${Math.floor(calPercent)}%</div>
+                    </div>
+                    <h3 class="values">Sugar</h3>
+                    <p>${sugars.toFixed(2)}g</p>
+                    <div class="w3-light-grey w3-round-xlarge">
+                      <div class="w3-container barFill w3-round-xlarge" style="width:${sugarPercent}%">${Math.floor(sugarPercent)}%</div>
+                    </div>
+                    <h3 class="values">Carbs</h3>
+                    <p>${carbs.toFixed(2)}g</p>
+                    <div class="w3-light-grey w3-round-xlarge">
+                      <div class="w3-container barFill w3-round-xlarge" style="width:${carbPercent}%">${Math.floor(carbPercent)}%</div>
+                    </div>
+                    <h3 class="values">Fat</h3>
+                    <p>${fat.toFixed(2)}g</p>
+                    <div class="w3-light-grey w3-round-xlarge">
+                      <div class="w3-container barFill w3-round-xlarge" style="width:${fatPercent}%">${Math.floor(fatPercent)}%</div>
+                    </div>
+                    <h3 class="values">Sodium</h3>
+                    <p>${sodium.toFixed(2)}mg</p>
+                    <div class="w3-light-grey w3-round-xlarge">
+                      <div class="w3-container barFill w3-round-xlarge" style="width:${sodiumPercent}%">${Math.floor(sodiumPercent)}%</div>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+        drinkIngStrList.forEach(elem => {
+          document.getElementById('ingredients').innerHTML += `
+                  <li class="collection-item">${elem}</li>
+                `
+        })
+      })
+      .catch(err => console.error(err))
+
+  }
+})
 
   // axios.get('https://api.edamam.com/api/nutrition-data?app_id=c543d3f7&app_key=b07975353995fafd647e1d5c37e04dc0&ingr=1%20large%20apple')
   //   .then(res => {
